5cf5ecf0e44f2daa3de6d3465a7595b4a08bd472
pandrew <letters@paulnotcom.se>
2014-01-25 20:50:47 +0100

    added functions to cleanup code

--- shapin.go
+++ shapin.go
@@ -8,7 +8,19 @@ import (
     "bufio"
     "log"
     "crypto/sha512"
+//    "github.com/howeyc/gopass"
 )
+func clearScreen () {
+    clearScreen := exec.Command("clear")
+    clearScreen.Stdout = os.Stdout
+    clearScreen.Run()
+}
+
+func clearClipboard () {
+    clearClipboard := exec.Command("/usr/bin/pbcopy", "<", "/dev/null")
+    clearClipboard.Stdout = os.Stdout
+    clearClipboard.Run()
+}
 
 func main() {
     reader := bufio.NewReader(os.Stdin)
@@ -16,18 +28,15 @@ func main() {
     if err != nil {
         log.Println(err)
         }
+    clearScreen()
+
+    //line := gopass.GetPasswd()
     b := sha512.Sum512([]byte(line))
     fmt.Println(fmt.Sprintf("%x", b))
 
     timer := time.NewTimer(10 * time.Second)
     <-timer.C
 
-
-    clearClipBoard := exec.Command("/usr/bin/pbcopy", "<", "/dev/null")
-    clearClipBoard.Stdout = os.Stdout
-    clearClipBoard.Run()
-    clearScreen := exec.Command("clear")
-    clearScreen.Stdout = os.Stdout
-    clearScreen.Run()
-
+    clearScreen()
+    clearClipboard()
 }
