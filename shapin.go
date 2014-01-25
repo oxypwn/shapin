1ba72b537b63e496b35ea6ab3532dd659a9c446b
pandrew <letters@paulnotcom.se>
2014-01-25 19:56:20 +0100

    10 second timer, clear clipboard, clear screen

--- shapin.go
+++ shapin.go
@@ -3,21 +3,31 @@ package main
 import (
     "fmt"
     "os"
+    "os/exec"
+    "time"
     "bufio"
+    "log"
     "crypto/sha512"
 )
 
 func main() {
     reader := bufio.NewReader(os.Stdin)
-    for {
-        line, err := reader.ReadString('\n')
+    line, err  := reader.ReadString('\n')
+    if err != nil {
+        log.Println(err)
+        }
+    b := sha512.Sum512([]byte(line))
+    fmt.Println(fmt.Sprintf("%x", b))
 
-        if err != nil {
-            break 
-            }
-        
+    timer := time.NewTimer(10 * time.Second)
+    <-timer.C
 
-        b := sha512.Sum512([]byte(line))
-        fmt.Println(fmt.Sprintf("%x", b))
-    }
+
+    clearClipBoard := exec.Command("/usr/bin/pbcopy", "<", "/dev/null")
+    clearClipBoard.Stdout = os.Stdout
+    clearClipBoard.Run()
+    clearScreen := exec.Command("clear")
+    clearScreen.Stdout = os.Stdout
+    clearScreen.Run()
+
 }
